5b04e536b1605c4147a6afb44a7768deb878984b
Malin Hanak <malinehanak@gmail.com>
2020-08-04 12:15:21 +0200

    new helpers

---
+++ utils/helper.js
@@ -0,0 +1,12 @@
+exports.createGameArray = (games) => {
+  return games.replace(/\s+/g, '').replace(/[ä]/g, 'a').replace(/[ö]/g, 'o').split(',');
+};
+
+exports.createScoreObject = (games) => {
+  const map = games.map((key) => [key, 0]);
+  return Object.fromEntries(map);
+};
+
+exports.createMemberArray = (members) => {
+  return members.replace(/, /g, ',').split(',');
+};
