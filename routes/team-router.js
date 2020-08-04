a0832e00225c1054c953c57eb8a36137e78362b2
Malin Hanak <malinehanak@gmail.com>
2020-08-04 17:16:30 +0200

    routers

--- routes/team-router.js
+++ routes/team-router.js
@@ -4,6 +4,7 @@ const teamControllers = require('../controllers/team-controllers');
 const teamRouter = express.Router();
 
 teamRouter.route('/').get(teamControllers.getTeams).post(teamControllers.create);
+teamRouter.route('/create-score').post(teamControllers.createScore);
 teamRouter.route('/set-score').patch(teamControllers.setScore);
 teamRouter.route('/get-score').get(teamControllers.getScore);
 teamRouter.route('/all-scores').get(teamControllers.getAllScores);
