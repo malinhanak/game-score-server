07541656669ac2af5c08d131a03205225d38a4a7
Malin Hanak <malinehanak@gmail.com>
2020-08-05 17:57:01 +0200

    trying to fix optiosn / preflight error

--- routes/team-router.js
+++ routes/team-router.js
@@ -1,11 +1,12 @@
 const express = require('express');
+var cors = require('cors');
 const teamControllers = require('../controllers/team-controllers');
 
 const teamRouter = express.Router();
 
 teamRouter.route('/').get(teamControllers.getTeams).post(teamControllers.create);
 teamRouter.route('/create-score').post(teamControllers.createScore);
-teamRouter.route('/set-score').patch(teamControllers.setScore);
+teamRouter.route('/set-score').patch(cors(), teamControllers.setScore);
 teamRouter.route('/get-score/:team').get(teamControllers.getScore);
 teamRouter.route('/all-scores').get(teamControllers.getAllScores);
 
