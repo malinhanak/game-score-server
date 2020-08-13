77474fad225a70e7f69de34b57fe6dec6eb32989
Malin Hanak <malinehanak@gmail.com>
2020-08-13 08:47:01 +0200

    sessonizedAdmin added

--- controllers/session-controllers.js
+++ controllers/session-controllers.js
@@ -4,7 +4,7 @@ const HttpError = require('../models/errors/HttpError');
 const Team = require('../models/team');
 const Admin = require('../models/admin');
 const { asyncWrapper } = require('../utils/asyncWrapper');
-const { sessionizeUser } = require('../utils/helpers');
+const { sessionizeUser, sessionizeAdmin } = require('../utils/helpers');
 
 const { SECRET_KEY } = process.env;
 
@@ -15,7 +15,7 @@ const login = async (req, res, next) => {
   if (!admin && !admin.comparePasswords(password)) {
     return next(new HttpError(`Användarnamn eller lösenord är felaktigt!`));
   }
-  const sessionUser = await sessionizeUser(admin);
+  const sessionUser = await sessionizeAdmin(admin);
   const token = jwt.sign({ ...sessionUser }, SECRET_KEY, { expiresIn: '1h' });
   res.status(200);
   return res.json({ ...sessionUser, token: token });
