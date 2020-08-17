a245f4e7baa2882011673026320e4b6fa4be847a
Malin Hanak <malinehanak@gmail.com>
2020-08-17 10:18:33 +0200

    old protection removed, will be replace

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -5,9 +5,6 @@ const Admin = require('../models/admin');
 const { asyncWrapper } = require('../utils/asyncWrapper');
 
 const create = async (req, res, next) => {
-  if (!req.admin || !req.session.admin.role.includes('ADMIN')) {
-    return next(new HttpError(`Du saknar behörighet`));
-  }
   const createAdmin = new Admin(req.body);
   await createAdmin.save();
 
@@ -15,4 +12,18 @@ const create = async (req, res, next) => {
   return res.json({ message: `Tävlingsledaren skapades!` });
 };
 
+const find = async (req, res, next) => {
+  const admins = await Admin.find({});
+
+  if (admins) {
+    res.status(200);
+    return res.json({
+      admins: admins.map((score) => score.toObject({ getters: true }))
+    });
+  }
+
+  return next(new HttpError(`Inga admins hittades`));
+};
+
 exports.create = asyncWrapper(create);
+exports.find = asyncWrapper(find);
